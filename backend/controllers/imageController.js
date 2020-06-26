67133c88fdfef1226741fe3b40854b50c7a3ffa4
Philip Crockett <philipmayoo@gmail.com>
2020-06-26 12:05:13 +0200

    Use `sharp` to resize images to 400x400
    Now use sharp for resizing images this required
    multer to use memory storage instead to access the buffer
    of a file object.
    
    resizeImage is now responsible for setting the save directory
    using sharp.toFile()

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,17 +1,11 @@
 const multer = require("multer");
 const ImageMetadata = require("../models/ImageMetadataModel");
+const sharp = require("sharp");
 
 const IMG_DIRECTORY_PATH = "public/img";
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, IMG_DIRECTORY_PATH);
-  },
-  filename: (req, file, cb) => {
-    const extension = file.mimetype.split("/")[1];
-    cb(null, `${req.body.name}.${extension}`);
-  },
-});
+// Use memory storage to access the file.buffer api and pass it to `sharp`
+const multerStorage = multer.memoryStorage();
 
 /**
  * Using the multers fileFilter to set what is allowed to upload to this web server.
@@ -24,7 +18,17 @@ function fileFilter(req, file, cb) {
   return cb(null, true);
 }
 
-function fileNamePath(name) {
+/**
+ * Formats the filename of the photo to the following:
+ * Only use the first 10 chars
+ * ÅÄÖ becomes A or O
+ * Spaces are replaced with dashes
+ *
+ * This function will also lowercase the name for
+ * making the matching of chars easier.
+ * @param {String} name
+ */
+function fileName(name) {
   let short = name.substring(0,10)
     .replace(/ /g, "-")
     .toLowerCase()
@@ -41,10 +45,51 @@ function fileNamePath(name) {
     date.getSeconds()
  }
 
+ /**
+  * Return an image name with the provided extension
+  * Uses split on the mimetype of the file, e.g `image/jpeg`
+  * @param {multer.file} file
+  * @param {String} name
+  */
+ function fileNameWithExtension(file, name) {
+   return `${fileName(name)}.${file.mimetype.split("/")[1]}`
+ }
+
+ /**
+  * A Complete path for a file and image
+  * This is used when saving the image at a specifc path
+  *
+  * See const IMG_DIRECTORY_PATH as the prefix for the image name
+  * @param {multer.file} file
+  * @param {String} name
+  */
+ function imagePath(file, name) {
+   return IMG_DIRECTORY_PATH + "/" + fileNameWithExtension(file, name)
+ }
+
 const upload = multer({
   storage: multerStorage,
   fileFilter: fileFilter
 });
+
+/**
+ * Resize the images provided using the `sharp` library
+ * This function is also responsible for saving the image
+ * under the correct path. See `imagePath` function
+ */
+exports.resizeImage = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+  const file = req.file;
+  const name = req.body.name;
+
+  await sharp(file.buffer)
+    .resize(400,400)
+    .toFile(imagePath(file, name))
+
+  next();
+}
 
 exports.getAllImages = async (req, res, next) => {
   const data = await ImageMetadata.find();
@@ -58,7 +103,7 @@ exports.getAllImages = async (req, res, next) => {
 exports.uploadImage = upload.single("photo");
 
 exports.createImageMetadata = async (req, res, next) => {
-  let name = fileNamePath(req.body.name)
+  let name = fileName(req.body.name)
   const doc = await ImageMetadata.create({
     name: name,
     path: `/img/${req.file.filename}`,
